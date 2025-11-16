f14afd7e5a1ff228b26332081d55a95e0be669c8
AnaCarolinaMoraesBelo <moraesbeloa@gmail.com>
2025-11-16 14:27:00 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+//import Login from "./componentes/props/Login.jsx"
+//import Login from "./componentes/state/Login.jsx"
+//import Login from "./componentes/CSS/Login.jsx"
+//import Login from "./componentes/tailwind/Login.jsx"
+//import Login from "./componentes/antd/Login.jsx"
+import PessoaForm from './componentes/cadastrapessoa/PessoaForm.jsx';
+
+function App() {
+  return (
+    <>
+      <PessoaForm />
+    </>
+  );
+}
+
+export default App;
